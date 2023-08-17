bf4286e26c597543350ebe94b4e99a0b94ad279e
Hameedullah Asadi <hameedasade20@gmail.com>
2023-08-17 13:18:09 -0700

    worked on react router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -4,13 +4,9 @@ To use router, we need to import a component named'BrowserRouter' which routes o
 
 To create anchor tags or links between 
 
-
-
-
-
 */
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom'
 import './App.css';
 //  pages
 import Home from './pages/Home.js';
@@ -23,16 +19,26 @@ function App() {
         inside this component, we can set our different routes and decides which component should be rendred 
         for each route.
     */
-  <BrowserRouter>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Job Router</h1>
+          <Link to="/">Home</Link>
+          <NavLink to="about">About</NavLink>
+          
+
+          
+
+
+          </nav>
+      </header>
       <main>
         {/* The 'Routes' is a parent component for multiple different route component and we set 
         a Route component for every route in our application. */}
         <Routes>
           {/* The following Route component is for homepage. In the elemtn, the component name goes. */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />              {/*We can also write the above code as follows: <Route index element=""{<Home />} />*/}
           <Route path="about" element={<About />} />
-
-
         </Routes>
     </main>
   </BrowserRouter>
